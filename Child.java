9036a2597cca6b08c6221f8cc46e55a9f8e91d82
JD <jayprowings@gmail.com>
2023-09-27 08:49:40 +0530

    initial commit

---
+++ Child.java
@@ -0,0 +1,15 @@
+package com.wings;
+
+public class Child extends Parent{
+
+	
+	int j = 20;
+	
+	public void show()
+	{
+		System.out.println("inside... Child show()");
+	}
+
+	//comment alskdj 
+	//Developer D2 - doing changes in child 
+}
